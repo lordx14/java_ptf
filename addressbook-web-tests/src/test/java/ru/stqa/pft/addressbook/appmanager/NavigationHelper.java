b65707c7e1a16f2e1b4d5772599b44707d21eab7
lordx14 <lordxxl@yandex.ru>
2019-08-17 17:45:46 +0300

    26 создан общий базовый класс для классов помощников

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/NavigationHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/NavigationHelper.java
@@ -6,14 +6,13 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 /**
  * Created by user on 017 17.08.19.
  */
-public class NavigationHelper {
-  private FirefoxDriver wd;
+public class NavigationHelper extends HelperBase {
 
   public NavigationHelper(FirefoxDriver wd) {
-    this.wd = wd;
+    super(wd);
   }
 
   public void gotoGroupPage() {
-    wd.findElement(By.linkText("groups")).click();
+    click(By.linkText("groups"));
   }
 }
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/SessionHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/SessionHelper.java
@@ -6,21 +6,16 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 /**
  * Created by user on 017 17.08.19.
  */
-public class SessionHelper {
-  private FirefoxDriver wd;
+public class SessionHelper  extends HelperBase {
 
   public SessionHelper(FirefoxDriver wd) {
-    this.wd = wd;
+    super(wd);
   }
 
   public void login(String username, String password) {
-    wd.findElement(By.name("user")).click();
-    wd.findElement(By.name("user")).clear();
-    wd.findElement(By.name("user")).sendKeys(username);
-    wd.findElement(By.name("pass")).click();
-    wd.findElement(By.name("pass")).clear();
-    wd.findElement(By.name("pass")).sendKeys(password);
-    wd.findElement(By.cssSelector("input[type=\"submit\"]")).click();
+    type(By.name("user"), username);
+    type(By.name("pass"), password);
+    click(By.cssSelector("input[type=\"submit\"]"));
   }
 
 }
