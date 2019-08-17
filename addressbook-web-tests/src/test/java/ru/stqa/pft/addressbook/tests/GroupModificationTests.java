c1dce4c1dbda557979d995e202e4837b32eb89d9
lordx14 <lordxxl@yandex.ru>
2019-08-17 19:54:41 +0300

    Сделал новый тест без использования рекордера, конец 2-го занятия

---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
@@ -0,0 +1,20 @@
+package ru.stqa.pft.addressbook.tests;
+
+import org.testng.annotations.Test;
+import ru.stqa.pft.addressbook.model.GroupData;
+
+/**
+ * Created by user on 017 17.08.19.
+ */
+public class GroupModificationTests extends TestBase {
+
+  @Test
+  public void testGroupModification() {
+    app.getNavigationHelper().gotoGroupPage();
+    app.getGroupHelper().selectGroup();
+    app.getGroupHelper().initgroupModification();
+    app.getGroupHelper().fillGroupForm(new GroupData("test1", "test2", "test3"));
+    app.getGroupHelper().submitGroupModification();
+    app.getGroupHelper().returnToGroupPage();
+  }
+}
