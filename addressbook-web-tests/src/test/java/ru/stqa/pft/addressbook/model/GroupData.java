f5b599fecd6841b32fcd9c86a8fa2873439c8865
lordx14 <lordxxl@yandex.ru>
2019-08-22 10:36:16 +0300

    после урока 44

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/GroupData.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/GroupData.java
@@ -1,18 +1,31 @@
 package ru.stqa.pft.addressbook.model;
 
 public class GroupData {
+  private final String id;
   private final String name;
   private final String header;
   private final String footer;
 
+  public GroupData(String id, String name, String header, String footer) {
+    this.name = name;
+    this.id = id;
+    this.header = header;
+    this.footer = footer;
+  }
+
   public GroupData(String name, String header, String footer) {
     this.name = name;
+    this.id = null;
     this.header = header;
     this.footer = footer;
   }
 
   public String getName() {
     return name;
+  }
+
+  public String getId() {
+    return id;
   }
 
   public String getHeader() {
@@ -22,4 +35,32 @@ public class GroupData {
   public String getFooter() {
     return footer;
   }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+
+    GroupData groupData = (GroupData) o;
+
+    if (id != null ? !id.equals(groupData.id) : groupData.id != null) return false;
+    return name != null ? name.equals(groupData.name) : groupData.name == null;
+
+  }
+
+  @Override
+  public int hashCode() {
+    int result = id != null ? id.hashCode() : 0;
+    result = 31 * result + (name != null ? name.hashCode() : 0);
+    return result;
+  }
+
+  @Override
+  public String toString() {
+    return "GroupData{" +
+            "name='" + name + '\'' +
+            ", id='" + id + '\'' +
+            '}';
+  }
+
 }
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
@@ -1,14 +1,21 @@
 package ru.stqa.pft.addressbook.tests;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupData;
+
+import java.util.List;
 
 public class GroupCreationTests extends TestBase {
 
   @Test
   public void testGroupCreation() {
     app.getNavigationHelper().gotoGroupPage();
-    app.getGroupHelper().createGroup(new GroupData("test1", null, null));
+    List<GroupData> before = app.getGroupHelper().getGroupList();
+        //app.getGroupHelper().createGroup(new GroupData("test1", "test2", "test3"));
+    app.getGroupHelper().createGroup(new GroupData("test1", "", ""));
+    List<GroupData> after = app.getGroupHelper().getGroupList();
+        Assert.assertEquals(after.size(), before.size() + 1);
   }
 
 }
